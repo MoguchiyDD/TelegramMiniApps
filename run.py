f9f82cbbac368f0af15d9594a4fcc382f50f75dc
MoguchiyDD <ket-ket-01@bk.ru>
2024-10-09 18:44:20 +0500

    fix: add socket (proto)

--- run.py
+++ run.py
@@ -3,12 +3,17 @@
 # run.py
 
 
-from asyncio import run
+from asyncio import run, gather
 from logging import basicConfig, INFO
 
 from aiogram import Bot, Dispatcher
 from app.game import router as gameRouter
 
+from fastapi import FastAPI, WebSocket
+from fastapi.middleware.cors import CORSMiddleware
+
+from uvicorn import Config, Server
+from threading import Event
 from dotenv import load_dotenv
 from os import getenv
 
@@ -17,15 +22,57 @@ load_dotenv()
 bot = Bot(token=getenv("TOKEN"))
 dp = Dispatcher()
 
+_app = FastAPI()
+_app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+stop_event = Event()
+
 
 async def main() -> None:
     dp.include_router(gameRouter)
-    await dp.start_polling(bot)
+    await gather(
+        dp.start_polling(bot, skip_updates=True),
+        start_fastapi_server()
+    )
+
+
+async def start_fastapi_server():
+    config = Config(app=_app, host=getenv("HOST"), port=int(getenv("PORT")))
+    server = Server(config)
+    await server.serve()
+
+
+@_app.get("/")
+async def read_root():
+    return {"message": "Welcome to the API"}
+
+
+@_app.websocket("/ws")
+async def websocket(websocket: WebSocket):
+    await websocket.accept()
+    try:
+        while True:
+            data = await websocket.receive_text()
+            print(f"Received message: {data}")
+            await websocket.send_text(f"Message received: {data}")
+    except Exception as e:
+        print(f"Connection closed with exception: {e}")
+    finally:
+        try:
+            await websocket.close()
+        except Exception as close_exception:
+            print(f"Error closing websocket: {close_exception}")
 
 
 if __name__ == "__main__":
     basicConfig(level=INFO)
     try:
         run(main())
-    except KeyboardInterrupt:
-        print("Exit")
+    except (KeyboardInterrupt, SystemExit):
+        print("Server and Bot stopped")
